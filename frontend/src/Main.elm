70b6189338bf01657efa705c72c304a5fef382da
Holodome <ilal.vinogradov@gmail.com>
2023-06-16 22:58:17 +0300

    frontend: add rendering of sudoku board

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,16 +1,59 @@
 module Main exposing (main)
 
+import Array
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import List exposing (head)
 import Sudoku exposing (..)
 
-view : String -> Html msg
-view data =
-    div []
-        [ h1 [] [ text data ]
-        ]
 
 
-main : Html msg
+-- MAIN
+
+
 main =
-    view "hello world"
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    SudokuBoard
+
+
+init : Model
+init =
+    let
+        width: Int 
+        width =
+            9
+
+        height : Int
+        height =
+            9
+    in
+    { cells = List.repeat height (List.repeat width 0)
+    , width = width
+    , height = height
+    }
+
+
+
+-- UPDATE
+
+
+update : msg -> Model -> Model
+update _ model =
+    model
+
+
+
+-- VIEW
+
+
+view : Model -> Html msg
+view =
+    viewSudoku
--- frontend/src/Sudoku.elm
+++ frontend/src/Sudoku.elm
@@ -1,67 +1,25 @@
-module Sudoku exposing (SudokuBoard)
+module Sudoku exposing (SudokuBoard, viewSudoku)
 
 import Array exposing (Array)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
-type alias SudokuCells =
-    Array Int
-
-
-type alias Location =
-    ( Int, Int )
-
-
 type alias SudokuBoard =
-    { cells : SudokuCells
+    { cells : List (List Int)
     , width : Int
     , height : Int
     }
 
 
-locationToIndex : SudokuBoard -> Location -> Int
-locationToIndex board ( x, y ) =
-    y * board.width + x
-
-
-sudokuAt : SudokuBoard -> Location -> Maybe Int
-sudokuAt board loc =
-    Array.get (locationToIndex board loc) board.cells
-
-
-sudokuSet : SudokuBoard -> Location -> Int -> SudokuBoard
-sudokuSet board loc value =
-    { board | cells = Array.set (locationToIndex board loc) value board.cells }
-
-
-viewSudokuCells : Int -> SudokuCells -> Int -> List (Html msg)
-viewSudokuCells width cells base =
-    let
-        nextBase =
-            base + width
-
-        row =
-            Array.slice base nextBase cells
-    in
-    if Array.isEmpty row then
-        []
-
-    else
-        (row
-            |> Array.toList
-            |> List.map (li [] << List.singleton << text << String.fromInt)
-            |> ul []
-        )
-            :: viewSudokuCells width cells nextBase
-
-
 viewSudoku : SudokuBoard -> Html msg
 viewSudoku board =
     let
-        rows =
-            viewSudokuCells board.width board.cells 0
+        viewRow : List Int -> Html msg
+        viewRow =
+            List.map (li [] << List.singleton << text << String.fromInt)
+                >> ul []
     in
-    rows
+    List.map viewRow board.cells
         |> List.map (li [] << List.singleton)
         |> ul []
